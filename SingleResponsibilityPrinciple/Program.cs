9cd660e5eede7e9c589d59534fae156e945f3f6d
cang8 <0.gizemgunes@gmail.com>
2024-10-17 18:14:38 +0300

    Add project files.

---
+++ SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SingleResponsibilityPrinciple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Single Responsibility Principle(SCP): A class should have only one responsibility," +
+                " and that responsibility should cover all functions of the class.");
+        }
+    }
+    public class Report_Correct
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class ReportPrinter
+    {
+        public void Print(Report_Correct report)
+        {
+            // print code
+        }
+    }
+
+    public class ReportSaver
+    {
+        public void Save(Report_Correct report)
+        {
+            // code save to database
+        }
+    }
+
+    // Report class only stores the report data, ReportPrinter does the printing process,
+    // and ReportSaver does the saving process. Each class has only one responsibility.
+}
---
+++ SingleResponsibilityPrinciple/Report.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class Report_Wrong
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public void PrintReport()
+        {
+            // print code
+        }
+
+        public void SaveReport()
+        {
+            // code save to database
+        }
+    }
+
+    // In this example, the Report class is responsible for both creating the report and printing and saving the report.
+    // This confuses the responsibilities of the class and can cause maintenance difficulties when changes are made.
+}
