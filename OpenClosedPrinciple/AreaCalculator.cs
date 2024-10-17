8101245630fa2194d32c720f2f2b413bb6de1535
cang8 <0.gizemgunes@gmail.com>
2024-10-17 18:46:12 +0300

    added-ocp

---
+++ OpenClosedPrinciple/AreaCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple
+{
+    public class AreaCalculator_Wrong
+    {
+        public double CalculateArea(object shape)
+        {
+            if (shape is Circle_Exp)
+            {
+                Circle_Exp circle = (Circle_Exp)shape;
+                return Math.PI * circle.Radius * circle.Radius;
+            }
+            else if (shape is Rectangle)
+            {
+                Rectangle rectangle = (Rectangle)shape;
+                return rectangle.Width * rectangle.Height;
+            }
+            throw new ArgumentException("Unknown shape");
+        }
+    }
+
+    // To add a new shape, it is necessary to modify the AreaCalculator class, which makes the class vulnerable to modification.
+}
---
+++ OpenClosedPrinciple/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace OpenClosedPrinciple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Open/Closed Principle(OCP): A class should be open to extension but closed to modification.");
+        }
+    }
+    public class Circle : IShape
+    {
+        public double _radius { get; set; }
+        public double CalculateArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+
+    public class Rectangle : IShape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public double CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    public class AreaCalculator_Correct
+    {
+        public double CalculateArea(IShape shape)
+        {
+            return shape.CalculateArea();
+        }
+    }
+
+    // The AreaCalculator class is open to be extended without modification to add new shapes, just using the IShape interface.
+}
